2e6ab436ec25d7f4b932d2c8b8156d5183baabc5
ryansouza <ryanp.souza@hotmail.com>
2024-10-23 19:50:13 -0300

    for javascript

---
+++ modulo-javascript-basico/05-operadores-logicos/index.js
@@ -0,0 +1,16 @@
+/*
+    == ---- igual ---- a==b ---- verdadeiro se "a" for igual a "b"
+    
+    === ---- idêntico ---- a===b ---- verdadeiro se "a" for identico a "b"
+    
+    != ---- diferente ---- a != b ---- verdadeiro se "a" for diferente de "b"
+
+    !== ---- não idêntico ---- a !== b ---- verdadeiro se o "a" não for idêntico a "b"
+*/
+
+const a = "3"
+const b = "3"
+
+console.log(a == b) //verdadeiro
+
+console.log(a === b) //falso
---
+++ modulo-javascript-basico/06-operadores-matematicos/index.js
@@ -0,0 +1,5 @@
+let primeiroValor = 6
+let segundoValor = "2"
+
+console.log(primeiroValor + segundoValor)
+//A "Soma" de um valor numero com um valor string é a sua concatenação, ou seja, nesse caso o resultado sera 62
