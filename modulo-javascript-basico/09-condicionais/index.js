65314bbfef3f74cda35c8ccfdcb0b8131c344656
ryansouza <ryanp.souza@hotmail.com>
2024-10-24 08:08:34 -0300

    condicional if else

---
+++ modulo-javascript-basico/09-condicionais/index.js
@@ -0,0 +1,12 @@
+let nomeFilme = "Vingadores"
+
+if(nomeFilme === "Vingadores"){
+    console.log("É o filme dos Vingadores")
+} else if (nomeFilme === "Batman") {
+    console.log("É o filme do Batman")
+} else {
+    console.log("É outro filme")
+}
+
+//Operador ternario
+true ? console.log("condicao true") : console.log("condicao false")
