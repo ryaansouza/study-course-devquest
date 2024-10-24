c5ee265b4702997ce712c685d273ecb289d67ecb
ryansouza <ryanp.souza@hotmail.com>
2024-10-24 11:49:51 -0300

    functions

---
+++ modulo-javascript-basico/11-funcoes/index.js
@@ -0,0 +1,12 @@
+function incentivarQuester(nomeQuester = "Quester"){
+    alert("Muito bem " + nomeQuester + "!! Você chegou ao JS")
+}
+
+incentivarQuester()
+
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultado = somar(1, 5)
+// console.log(resultado)
