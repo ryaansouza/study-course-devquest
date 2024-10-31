0324671aa6ab61a25298f2971576d8d95a4918b9
ryansouza <ryanp.souza@hotmail.com>
2024-10-31 07:43:01 -0300

    tipos de escopos

---
+++ modulo-javascript-intermediario/01-escopo-no-javascript/index.js
@@ -0,0 +1,28 @@
+// if (true) {
+//     const mensagem = 'Olá'
+//     console.log(mensagem) //Vai funcionar
+// }
+// console.log(mensagem); //Não vai funcionar
+
+
+// {
+//     //BLOCO DE CÓDIGO AUTÓNOMO
+//     const mensagem = 'Olá'
+//     var tipoVar = 'Sou uma variavel tipo var'
+
+//     console.log(mensagem) //Vai funcionar
+//     console.log(tipoVar); //Vai funcionar
+    
+// }
+// console.log(mensagem); //Não vai funcionar
+// console.log(tipoVar); //Vai funcionar
+
+// function falar() {
+//     var mensagem = 'Ola Mundo!'
+//     console.log(mensagem);
+// }
+
+// falar()
+// console.log(mensagem);
+
+console.log(url);
