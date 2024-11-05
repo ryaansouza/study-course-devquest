35b68efb2772afb6acc41a281265988b060129a5
ryansouza <ryanp.souza@hotmail.com>
2024-11-05 18:22:02 -0300

    arrow-functions

---
+++ modulo-javascript-intermediario/04-arrow-functions/index.js
@@ -0,0 +1,18 @@
+//Quando tiver duas ou mais entradas
+// let resuldadoDaSoma = (numero1, numero2) => {
+//     return numero1 + numero2
+// }
+
+// console.log(resuldadoDaSoma(12, 5));
+
+//Quando tiver apenas uma entrada
+// const incentivarQuester = nomeQuester => {
+//     console.log('Parabéns ' + nomeQuester+ ', Você chegou ao módulo de JS intermediário');
+// }
+
+// incentivarQuester('Ryan')
+
+//Quando tiver apenas uma linha de código de execução
+let resuldadoDaSoma = (numero1, numero2) => numero1 + numero2
+
+console.log(resuldadoDaSoma(12, 5));
