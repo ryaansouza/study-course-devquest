24bf6b1a65384e89ac37aac76171a67e93eba865
ryansouza <ryanp.souza@hotmail.com>
2024-11-05 18:10:42 -0300

    funções anonimas

---
+++ modulo-javascript-intermediario/03-funcoes-anonimas/index.js
@@ -0,0 +1,13 @@
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(12, 5)
+// console.log(resultadoDaSoma);
+
+let resuldadoDaSoma = function(numero1, numero2){
+    return numero1 + numero2
+}
+
+console.log(resuldadoDaSoma);
+console.log(resuldadoDaSoma(12, 5));
