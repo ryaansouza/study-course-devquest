0e13df3759a80206d55f52b2b23d49def7028cec
ryansouza <ryanp.souza@hotmail.com>
2024-11-06 18:20:14 -0300

    trabalhando com arrays

---
+++ modulo-javascript-intermediario/08-trabalhando-com-arrays/index.js
@@ -0,0 +1,26 @@
+let frutas = ['banana', 'maçã', 'laranja', 'pêra']
+
+//Para adicionar um novo elemento no array
+frutas.push('manga')
+
+//Para tirar o ultimo elemento do array
+frutas.pop()
+
+//Para remover o primeiro elemento do array
+frutas.shift()
+
+//Adicionar um elemento no começo do array
+frutas.unshift('morango')
+
+//Procura o index do item no array
+let posicaoLaranja = frutas.indexOf('laranja')
+
+// for (let i = 0; i < frutasTamanho; i++) {
+//     console.log('Fruta: ' + frutas[i]);
+// }
+
+// frutas.forEach(function(indice, item, array) {
+//     console.log(item);
+//     console.log(indice);
+//     console.log(array);
+// });
