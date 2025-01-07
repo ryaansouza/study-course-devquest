c0c2295a2fb5a282daf4067057444599f410c2d0
ryansouza <ryanp.souza@hotmail.com>
2025-01-07 18:01:17 -0300

    comentarios no arquivo js

--- modulo-javascript-intermediario/17-manipulando-html-dom/index.js
+++ modulo-javascript-intermediario/17-manipulando-html-dom/index.js
@@ -1,9 +1,12 @@
 function adicionarComentario(){
     let inputNovoComentario = document.getElementsByName('novo-comentario');
 
+    //conseguindo o texto digitado no input de novo comentario
     let textoDigitado = inputNovoComentario[0].value;
 
+    //pegando o elemento html que ira receber os comentarios
     let novosComentarios = document.getElementById('novos-comentarios');
 
+    //adicionando html ao Inner do elemento que ira receber comentarios
     novosComentarios.innerHTML += `<p>Novo comentário: ${textoDigitado} </p>`
 }
