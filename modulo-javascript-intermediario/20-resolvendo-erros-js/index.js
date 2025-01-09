b81e723f5ffda84c3cea457321867d640edd05dc
ryansouza <ryanp.souza@hotmail.com>
2025-01-09 17:42:25 -0300

    encontrando e interpretando erros no js

---
+++ modulo-javascript-intermediario/20-resolvendo-erros-js/index.js
@@ -0,0 +1,11 @@
+// let valorBoleto = 100;
+
+// console.log(valorBolet);
+
+let inputTelefone = document.getElementById('telefone');
+
+inputTelefone.disabled = true;
+
+function habilitarTelefone (){
+    inputTelefone.disabled = false;
+}
